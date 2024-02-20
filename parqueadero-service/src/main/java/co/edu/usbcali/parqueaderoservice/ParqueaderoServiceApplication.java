9131b44f38bfa44945c7b4f7e1a3e62193700d20
bolanosp <73298224+bolanosp@users.noreply.github.com>
2024-02-19 19:36:34 -0500

    Add files via upload

---
+++ parqueadero-service/src/main/java/co/edu/usbcali/parqueaderoservice/ParqueaderoServiceApplication.java
@@ -0,0 +1,13 @@
+package co.edu.usbcali.parqueaderoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParqueaderoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParqueaderoServiceApplication.class, args);
+	}
+
+}
---
+++ parqueadero-service/src/test/java/co/edu/usbcali/parqueaderoservice/ParqueaderoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.usbcali.parqueaderoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParqueaderoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
