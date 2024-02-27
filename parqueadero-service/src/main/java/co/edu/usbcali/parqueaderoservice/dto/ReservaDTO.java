ee3303516a2df452efbbf572ae79bd20bc2f9991
Juan Diego Bolaños <juandy80@hotmail.com>
2024-02-26 19:16:48 -0500

    Todos los repositorios

---
+++ parqueadero-service/src/main/java/co/edu/usbcali/parqueaderoservice/dto/ReservaDTO.java
@@ -0,0 +1,22 @@
+package co.edu.usbcali.parqueaderoservice.dto;
+
+import co.edu.usbcali.parqueaderoservice.models.Parqueadero;
+import co.edu.usbcali.parqueaderoservice.models.Vehiculo;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReservaDTO {
+    private Integer id;
+    private Date horaEntrada;
+    private String estado;
+    private Vehiculo vehiculo;
+    private Parqueadero parqueadero;
+}
+}
