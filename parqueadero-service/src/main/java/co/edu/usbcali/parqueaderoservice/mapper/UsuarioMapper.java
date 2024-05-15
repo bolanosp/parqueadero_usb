c4428cc68072794aac9132eaf29f2d01f3f63989
vestupinan <victor.estupinan96@gmail.com>
2024-05-15 14:59:27 -0500

    Se agregar el mapeo de listas al mapper

--- parqueadero-service/src/main/java/co/edu/usbcali/parqueaderoservice/mapper/UsuarioMapper.java
+++ parqueadero-service/src/main/java/co/edu/usbcali/parqueaderoservice/mapper/UsuarioMapper.java
@@ -1,36 +1,42 @@
 package co.edu.usbcali.parqueaderoservice.mapper;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import co.edu.usbcali.parqueaderoservice.dto.UsuarioDto;
 import co.edu.usbcali.parqueaderoservice.models.Usuario;
 
 public class UsuarioMapper {
 
     public static UsuarioDto toUserDTO(Usuario usuario) {
-            if (usuario == null) {
-                return null;
-            }
-    
-            UsuarioDto usuarioDto = new UsuarioDto();
-            usuarioDto.setId(usuario.getId());
-            usuarioDto.setNombre(usuario.getNombre());
-            usuarioDto.setApellido(usuario.getApellido());
-            usuarioDto.setCorreo_electronico(usuario.getCorreo_electronico());
-    
-            return usuarioDto;
+        if (usuario == null) {
+            return null;
         }
+
+        UsuarioDto usuarioDto = new UsuarioDto();
+        usuarioDto.setId(usuario.getId());
+        usuarioDto.setNombre(usuario.getNombre());
+        usuarioDto.setApellido(usuario.getApellido());
+        usuarioDto.setCorreo_electronico(usuario.getCorreo_electronico());
+
+        return usuarioDto;
+    }
 
     public static Usuario toUser(UsuarioDto usuarioDto) {
-            if (usuarioDto == null) {
-                return null;
-            }
-    
-            Usuario usuario = new Usuario();
-            usuario.setId(usuarioDto.getId());
-            usuario.setNombre(usuarioDto.getNombre());
-            usuario.setApellido(usuarioDto.getApellido());
-            usuario.setCorreo_electronico(usuarioDto.getCorreo_electronico());
-    
-            return usuario;
+        if (usuarioDto == null) {
+            return null;
         }
 
+        Usuario usuario = new Usuario();
+        usuario.setId(usuarioDto.getId());
+        usuario.setNombre(usuarioDto.getNombre());
+        usuario.setApellido(usuarioDto.getApellido());
+        usuario.setCorreo_electronico(usuarioDto.getCorreo_electronico());
+
+        return usuario;
+    }
+
+    public static List<UsuarioDto> userToUsersDTO(List<Usuario> usuarios) {
+        return usuarios.stream().map(UsuarioMapper::toUserDTO).collect(Collectors.toList());
+    }
 }
